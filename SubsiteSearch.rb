1ac4987dc6e7407069b590b85632fb578d8a59ef
akorelkov <akorelkov@codehq.cynaptic.eu>
2014-10-14 12:57:46 +0700

    added count_indegree lambda

--- SubsiteSearch.rb
+++ SubsiteSearch.rb
@@ -2,10 +2,28 @@ begin
 
   require ('open-uri')
 
-  #Gets the URL string from user
-  p "Please, input URL address the following format 'http://test.com':"
-  url = gets
-  url = url.chomp.to_s
+  def look_for_subsite(url)
+    subsite = open(url).read.
+      scan(/(?<=href=")#{url}\/[\w\d\-]+/).uniq
+    subsite << url
+  end
+
+  #Count Indegree. 
+  count_indegree = lambda do |array_of_subsites| 
+    hash_of_indegree = {}
+    array_of_subsites.map do |url|
+      array_of_subsites.map do |item|
+         if(item != url)
+           check = open(url).read =~ /(?<=href=")#{item}[\/]?(?=")"/
+           unless check.nil?
+             hash_of_indegree[item] ||= 0
+             hash_of_indegree[item] = hash_of_indegree[item] + 1
+           end 
+         end
+      end
+    end
+    hash_of_indegree
+  end
 
   #Validate URL. Generate Exception if user to input not URL.
   #param url - string
@@ -16,17 +34,21 @@ begin
     end 
   end
 
-  def look_for_subsite(url)
-    subsite = open(url).read.
-      scan(/#{url}\/[\w\d]+/).uniq
-    print subsite
-  end
+  #Gets the URL string from user
+  p "Please, input URL address the following format 'http://test.com':"
+  url = gets
+  url = url.chomp.to_s
 
   validate_url.call(url)
   rescue Exception => e 
     puts e.message  
   else
 
-  look_for_subsite(url)
+  #Remove trailing slash
+  url = url.gsub(/\/$/,"") 
+
+  array_of_subsites = look_for_subsite(url)
+
+  p Hash[count_indegree.(array_of_subsites).sort_by{ |key, value| -value }]
 
 end
