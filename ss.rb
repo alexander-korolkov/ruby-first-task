3b9a44215c6c3b6445db85a6d85cd6440fb1ea46
ilyakhlyzov <khlyzoff@gmail.com>
2014-10-16 06:37:05 +0400

    modified regexp, new functions to parse links

--- ss.rb
+++ ss.rb
@@ -6,12 +6,22 @@ url = URI.parse($stdin.gets).normalize
 
 unless url.absolute?() then raise "url is not absolute" end 
 
-gen_parse_links = ->(url) do
-  ->(text) do
-    text.scan(Regexp.new(/<a[^>]+href="(?:#{url.scheme}:)?(?:\/\/#{url.host})?([^?"]+)/))
-  end
+read_page = ->(url) do
+  url.read
 end
 
-text = "<a http://"
+parse_all_links = ->(url) do
+  read_page.call(url).scan(/<a[^>]+href="([^?\#"]+)"/).uniq
+end
 
-gen_parse_link.call(url).call(text)
+p parse_all_links.call(url)
+
+gen_parse_links = ->(url) do 
+  ->(smth) do
+    smth.to_s.gsub(/"(?:(?<path>\/[\.\w\d-]+)+)/, "#{url.scheme}://#{url.host}#{url.path}" << '\k<path>').
+      gsub(/"(\/\/)/, "http://").
+      scan(/#{url.scheme}:\/\/#{url.host}[\w\d\/-]+/)
+  end  
+end
+
+p gen_parse_links.call(url).call(parse_all_links.call(url))
